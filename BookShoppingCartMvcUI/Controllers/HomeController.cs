8700437f06f1875d42a69b4b1f3988912795c49d
samikshaaghodke <samikshaaghodke@gmail.com>
2024-03-20 11:27:40 +0100

    Add project files.

---
+++ BookShoppingCartMvcUI/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using BookShoppingCartMvcUI.Models;
+using BookShoppingCartMvcUI.Models.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BookShoppingCartMvcUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHomeRepository _homeRepository;
+
+        public HomeController(ILogger<HomeController> logger, IHomeRepository homeRepository)
+        {
+            _homeRepository = homeRepository;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index(string sterm = "", int genreId = 0, string returnUrl =null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+          
+            if (!User.Identity.IsAuthenticated)
+            {
+                // Construct the root-relative path to the Login page
+                string loginUrl = Url.Content("~/Identity/Account/Login");
+
+                // Redirect to the login page
+                return Redirect($"{loginUrl}?ReturnUrl={Uri.EscapeDataString(returnUrl)}");
+            }
+
+            // If authenticated, proceed with the normal logic for the Index action
+
+            IEnumerable<Book> books = await _homeRepository.GetBooks(sterm, genreId);
+            IEnumerable<Genre> genres = await _homeRepository.Genres();
+            BookDisplayModel bookModel = new BookDisplayModel
+            {
+                Books = books,
+                Genres = genres,
+                STerm = sterm,
+                GenreId = genreId
+            };
+            return View(bookModel);
+        }
+
+        public IActionResult Return()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
